beac8dd3f89d72406913fa5e0e84679912e1c0dc
Erik P. Williams <epwilliams@csu.fullerton.edu>
2024-03-26 20:39:58 -0700

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -1,17 +1,1 @@
-#include <iostream>
-#include <fstream>
-int calculate_sum(int a, int b) {return a + b;}
-int main() {
-    std::ifstream inputFile("input.txt"); 
-    int num1, num2;
-    if (!inputFile) {
-        std::cerr << "Unable to open file" << std::endl;
-        return 1;
-    }
-    inputFile>>num1>>num2;
-    inputFile.close();
-    int result = calculate_sum(num1, num2);
-    std::cout<<"Sum: "<<result<<std::endl;
-    return 0;
-}
 
